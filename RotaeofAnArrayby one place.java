21fe0191f8a3d0e66570dd95308de71be620c4a9
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-28 13:04:02 +0530

    Create RotaeofAnArrayby one place.java

---
+++ RotaeofAnArrayby one place.java
@@ -0,0 +1,11 @@
+static int[] rotateArray(int[] arr, int n) {
+        // Write your code here.
+
+        int temp = arr[0];
+        for(int i=0;i<n;i++){
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+        return arr;
+    }
+}
